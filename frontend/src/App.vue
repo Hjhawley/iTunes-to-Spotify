a1c7b1f181b57cdf41020c4392948ebea49e9b11
Spaghettatron <austin.lamoreaux@gmail.com>
2025-07-14 09:40:25 -0600

    Merge Cleanup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,9 +2,9 @@
 import { ref, watch, nextTick, onMounted } from "vue";
 
 const user = ref(null);
-const file = ref(null);       // the uploaded XML
-const log = ref(null);        // the DOM element reference of the status log
-const logMessages = ref([]);  // array of log messages (strings)
+const file = ref(null); // the uploaded XML
+const log = ref(null); // the DOM element reference of the status log
+const logMessages = ref([]); // array of log messages (strings)
 const status = ref([]);
 const uris = ref([]);
 
@@ -95,7 +95,8 @@ async function onSubmit() {
 }
 
 watch(
-  () => logMessages.value.length, async () => {
+  () => logMessages.value.length,
+  async () => {
     await nextTick();
     if (log.value) {
       log.value.scrollTop = log.value.scrollHeight;
@@ -105,21 +106,20 @@ watch(
 </script>
 
 <template>
-  <div class="container">
-    <!-- main-content: login and buttons -->
-    <div class="main-content">
-      <h1>iTunes &gt;&gt; Spotify<br/>Playlist Migrator</h1>
+  <!-- main-content: login and buttons -->
+  <div class="main-content">
+    <h1>iTunes &gt;&gt; Spotify<br />Playlist Migrator</h1>
 
-      <!-- not yet authenticated -->
-      <div v-if="!user">
-        <button @click="loginWithSpotify">Login with Spotify</button>
-      </div>
+    <!-- not yet authenticated -->
+    <div v-if="!user">
+      <button @click="loginWithSpotify">Login with Spotify</button>
+    </div>
 
-      <!-- once logged in -->
-      <div v-else class="user-info">
-        <img v-if="user.images?.length" :src="user.images[0].url" />
-        <p>Logged in as {{ user.display_name }}</p>
-        <button @click="logoutWithSpotify">Log out of Spotify</button>
+    <!-- once logged in -->
+    <div v-else class="user-info">
+      <img v-if="user.images?.length" :src="user.images[0].url" />
+      <p>Logged in as {{ user.display_name }}</p>
+      <button @click="logoutWithSpotify">Log out of Spotify</button>
 
       <p>Upload your iTunes XML playlist:</p>
       <input type="file" accept=".xml,text/xml" @change="onFileSelect" />
@@ -153,20 +153,21 @@ watch(
       </div>
     </div>
 
-      <!-- status log -->
-      <div class="status-log" v-if="user" ref="log">
-        <p v-for="(msg, i) in logMessages" :key="i">{{ msg }}</p>
-      </div>
+    <!-- status log -->
+    <div class="status-log" v-if="user" ref="log">
+      <p v-for="(msg, i) in logMessages" :key="i">{{ msg }}</p>
+    </div>
   </div>
 
   <!-- footer -->
   <footer>
-    This web app is not affiliated with Apple or Spotify.<br/>
+    This web app is not affiliated with Apple or Spotify.<br />
     Source code available at
     <a
       href="https://github.com/Hjhawley/iTunes-to-Spotify"
       target="_blank"
       rel="noopener"
-    >github.com/Hjhawley/iTunes-to-Spotify</a>
+      >github.com/Hjhawley/iTunes-to-Spotify</a
+    >
   </footer>
 </template>
